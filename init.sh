169a7b18311018816cb4ae8ccb4962ca1c85ed2f
Aathreya Sharma <aathreyasharma@outlook.com>
2020-01-02 00:59:01 +0530

    first commit

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Added Environments.."
+echo "export dbName=angularAuth
+export dbPort=27017
+export serverPort=3000
+export saltRounds=10" >> ~/.bashrc
+
---
+++ nodeApi/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const api = require('./routes/api')
+const cors = require('cors')
+const port = 3000
+
+const app = express()
+app.use(cors())
+
+app.use(bodyParser.json())
+
+app.use('/api', api)
+
+app.get('/', function(req, res){
+	res.send('Hello from Server')
+})
+
+app.listen(port, function(){
+	console.log('Server running on localhost: ' + port)
+})
